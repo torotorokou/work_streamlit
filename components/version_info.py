c8882b3d92852c89262738c5f22655bcd4adf1ff
torotorokou <happy.go.lucky.ssd@gmail.com>
2025-04-15 04:42:31 +0000

    トップページ終了

--- components/version_info.py
+++ components/version_info.py
@@ -5,8 +5,8 @@ def show_version_info():
     st.subheader("🔖 バージョン・サポート情報")
     st.markdown(
         """
-    - **バージョン**：`v1.0.1`（最終更新：2025-04-04）  
-    - **開発・管理**：黒木 土田
+    - **バージョン**：`v1.0.1`（最終更新：`2025-04-15`）
+    - **開発・管理**：黒木・土田
     - **お問い合わせ**：[support@example.com](mailto:support@example.com)
     """
     )
