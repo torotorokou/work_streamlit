86387c1ab876729ded622e99e7517a92883e0859
torotorokou <happy.go.lucky.ssd@gmail.com>
2025-04-21 04:15:36 +0000

    configのクラス化は取りやめ

---
+++ logic/config_loader/base_loader.py
@@ -0,0 +1,9 @@
+class BaseConfigLoader:
+    def __init__(self, path_config_file = "config/main_paths.yaml"):
+        self.paths = self.load_yaml(path_config_file)
+
+    def _load_yaml(self, path: str) -> dict:
+        from pathlib import Path
+        import yaml
+        with open(Path(path), encoding="utf-8") as f:
+            return yaml.safe_load(f)
