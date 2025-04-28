66b5e4a3a72fcad6591da044f88c34a8521a0e87
torotorokou <happy.go.lucky.ssd@gmail.com>
2025-04-28 22:51:28 +0000

    wip/開発環境の構築

---
+++ app/config/settings/dev.py
@@ -0,0 +1,7 @@
+from config.settings.base import *
+
+# --- 開発環境用設定 ---
+
+DEBUG = True
+ENV_NAME = "development"
+STREAMLIT_SERVER_PORT = 8503  # 開発用ポート
---
+++ app/config/settings/prod.py
@@ -0,0 +1,7 @@
+from config.settings.base import *
+
+# --- 本番環境用設定 ---
+
+DEBUG = False
+ENV_NAME = "production"
+STREAMLIT_SERVER_PORT = 8501
